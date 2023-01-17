8d92ccfaf85aa0f99de0322fc2cf50cd18958531
Alexander Angelov <alexander.angelov@mentormate.com>
2023-01-17 18:47:33 +0200

    Initial Commit

---
+++ SignInScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SignInScreen
+//
+//  Created by Alexander Angelov on 17.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
